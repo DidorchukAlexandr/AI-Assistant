259af663e281199011cac67f7ce44b8111ce21f2
DidorchukAlexandr <104354949+DidorchukAlexandr@users.noreply.github.com>
2024-10-01 16:43:56 +0300

    add

--- src/components/Register/validationStatus.jsx
+++
@@ -1,9 +0,0 @@
-// import { ErrorText, SuccessText } from './validationStatus.styled';
-
-// export const ValigationStatus = ({ name = '', error = '', success = '' }) => {
-//   if (name && error) {
-//     return <ErrorText> {error} </ErrorText>;
-//   } else if (name && !error) {
-//     return <SuccessText>{success}</SuccessText>;
-//   } else return <></>;
-// };
